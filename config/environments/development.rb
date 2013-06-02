091274f74023e36de599e0806521f77f2f4b21e6
Christophe1 <harris.christophe@gmail.com>
2013-06-02 23:01:59 +0100

    progress on the Mailer. Although some suspicious activity

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,19 +14,25 @@ QuestionnaireSite::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+
+   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
       :port                 => 587,
-      :domain               => 'gmail.com',
-      :user_name            => 'questionnaire.dev@gmail.com',
-      :password             => 'qwerty_123',
+      # :domain               => 'gmail.com',
+      :user_name            => 'harris.christophe@gmail.com',
+      :password             => 'hilaire2',
       :authentication       => 'plain',
       :enable_starttls_auto => true
   }
+
+
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
