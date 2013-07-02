a135aa9f4d0f5ea17b8f572b22f4d2a9eabeae48
Christophe1 <harris.christophe@gmail.com>
2013-07-02 09:44:49 +0100

    css changes to character

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -18,7 +18,7 @@ var ajax_loaded = (function(response) {
 
 if ($("#map_canvas").length > 0)
 {
-  console.log('ajax 2 - map_canvas is detected'); 
+  // console.log('ajax 2 - map_canvas is detected'); 
   initialize_google_maps();
 
 }
@@ -165,16 +165,31 @@ function initialize_google_maps() {
 
     var currentlatlng = new google.maps.LatLng(user_latitude, user_longitude);
 
+
+
     var zoom = 10;
     var myOptions = {
       zoom: zoom,
       center: currentlatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP, // ROADMAP, SATELLITE, HYBRID
-        streetViewControl: false
+        streetViewControl: false,
+        minZoom: 0, 
+        // don't want people to be able to hone in on others' addresses too specifically.
+        maxZoom: 13
       };
+
+
       map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
+      // if you don't want a marker, uncomment the line of code below,
+      // and comment out the 3 lines after it.
+// to make the markers invisible, set oppacity to zero
       var marker = new google.maps.Marker({map: map, position: currentlatlng, icon:{oppacity:0}});
+    //    var marker = new google.maps.Marker({map: map, position: currentlatlng});
+    // map.setCenter(currentlatlng);
+    // map.setZoom(zoom);
+
+
 
       var circle = new google.maps.Circle({
         map: map,
@@ -186,23 +201,23 @@ function initialize_google_maps() {
       circle.bindTo('center', marker, 'position');
     }
 
-function show_markers() {
-  console.log("show markers"); 
-  if (markers)
-    for(i in markers) {
-      markers[i].setMap(map);
-    }
-  }
+// function show_markers() {
+//   // console.log("show markers"); 
+//   if (markers)
+//     for(i in markers) {
+//       markers[i].setMap(map);
+//     }
+//   }
 
-  function add_marker(location) {
-    console.log("add marker");
-    marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-    markers.push(marker);
-    markers.setVisible(false);
-  }
+  // function add_marker(location) {
+  //   // console.log("add marker");
+  //   marker = new google.maps.Marker({
+  //     position: location,
+  //     map: map
+  //   });
+  //   markers.push(marker);
+  //   markers.setVisible(true);
+  // }
 
 
 
