0808195be672336e73cd461c6f648084fd45a4b1
Rosca Sergiu <rmagnum2002@gmail.com>
2013-08-25 18:48:06 +0300

    mailer settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,20 @@ QuestionnaireSite::Application.configure do
 
   # Need this here for mailing purposes
   config.action_mailer.default_url_options = { :host => 'populisto.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "populisto.com",
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
+  }
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
