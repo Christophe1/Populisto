9eb67e58e056cafb0115dba42bdd55c69e85d55f
Rosca Sergiu <rmagnum2002@gmail.com>
2013-12-02 19:59:18 +0200

    fixed your address book design

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,9 +7,8 @@ set :output, "log/cron_log.log"
 set :environment, 'production'
 
 # every :day, :at => '3:00 am' do
-every 5.days  do
-  # runner "EmailInvitesReminder.perform()"
-  runner "EmailInvitesReminder.perform"
+every 5.days, :at => '3:00 am' do
+  runner "EmailInvitesReminder.perform()"
 end
 
 # Learn more: http://github.com/javan/whenever
